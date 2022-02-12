2da6d537e95f4bf713e90d697e92fa02cf36a998
Panchuel <danniel928@gmail.com>
2022-02-11 21:23:22 -0500

    Primer intento de muerte

--- problem/program.asm
+++ problem/program.asm
@@ -1,6 +1,52 @@
-// COLOCO AQUÍ TU NOMBRE COMPLETO
-// COLOCA AQUÍ TU ID
-// COLOCA AQUÍ TU CORREO ELECTRÓNICO
+// Jose Daniel Montoya Molina
+// 000419838
+// jose.montoyam@upb.edu.co
 (START)
-    @START
-    0;JMP
+@16384
+D = A
+@1
+M = D
+@1
+D = M
+@2
+D = M
+@24576
+D = M
+@70
+D = D - A
+@4
+D;JEQ
+@24576
+D = M
+@67
+D = D - A
+@4
+D;JEQ
+@22
+0;JMP
+@2
+D = M
+@24576
+D = D - A
+@4
+D;JGT
+@2
+A = M
+M = -1
+@2
+M = M+1
+@22
+0;JMP
+@2
+D = M
+@24576
+D = D - A
+@4
+D;JGT
+@2
+A = M
+M = 0
+@2
+M = M+1
+@35
+0;JMP
